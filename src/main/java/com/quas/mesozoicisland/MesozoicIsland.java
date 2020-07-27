d415995df3f5dea8d7738553432d4903cbd8679f
Quas <ctsn99@gmail.com>
2020-07-27 14:55:27 -0400

    Testing JDA

--- src/main/java/com/quas/mesozoicisland/MesozoicIsland.java
+++ src/main/java/com/quas/mesozoicisland/MesozoicIsland.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
-public class Main {
+public class MesozoicIsland {
 
 	public static JDA jda;
 	
