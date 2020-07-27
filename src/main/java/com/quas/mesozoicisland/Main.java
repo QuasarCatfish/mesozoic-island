c93bcdbe86fedc88795e63ca25938a68210203c4
Quas <ctsn99@gmail.com>
2020-07-27 05:52:29 -0400

    Testing JDA

--- src/main/java/com/quas/mesozoicisland/Main.java
+++ src/main/java/com/quas/mesozoicisland/Main.java
@@ -2,6 +2,8 @@ package com.quas.mesozoicisland;
 
 import javax.security.auth.login.LoginException;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.ReadyEvent;
