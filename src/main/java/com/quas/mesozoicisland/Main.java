122e5f5a187989aa33ff63155be137ce4f63634b
Quas <ctsn99@gmail.com>
2020-07-27 05:29:55 -0400

    Testing JDA

--- src/main/java/com/quas/mesozoicisland/Main.java
+++ src/main/java/com/quas/mesozoicisland/Main.java
@@ -1,102 +1,40 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.quas.mesozoicisland;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
+import javax.security.auth.login.LoginException;
 
-import javax.measure.quantity.Mass;
-import javax.measure.unit.SI;
-import javax.sql.DataSource;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-import org.jscience.physics.amount.Amount;
-import org.jscience.physics.model.RelativisticModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+	public static JDA jda;
 
-  @Autowired
-  private DataSource dataSource;
+	public static void main(String[] args) throws LoginException, InterruptedException {
+		jda = JDABuilder.create("NjQ0MzQ3MTM3NzIyODEwMzY4.XrHFqw.Hdy_ftzhp-0Ju-aes2YnSMS13U8",
+				GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS)).addEventListeners(new ListenerAdapter() {
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
+					@Override
+					public void onReady(ReadyEvent event) {
+						System.out.println("Bot Loaded: " + event.getJDA().getSelfUser().getName());
+					}
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
+					@Override
+					public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+						if (event.getMessage().getContentRaw().equalsIgnoreCase("!quit")) {
+							shutdown();
+						}
+					}
+				}).build().awaitReady();
 
-  @RequestMapping("/hello")
-  String hello(Map<String, Object> model) {
-    RelativisticModel.select();
-    Amount<Mass> m = Amount.valueOf("12 GeV").to(SI.KILOGRAM);
-    model.put("science", "E=mc^2: 12 GeV = " + m.toString());
-    return "hello";
-  }
+		System.out.println("Ready!");
+	}
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
-
+	public static void shutdown() {
+		jda.shutdown();
+		System.exit(0);
+	}
 }
