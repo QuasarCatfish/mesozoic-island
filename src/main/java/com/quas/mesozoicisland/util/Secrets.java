9234460438f66539f1b2787b34bc3ed8b5f4f85e
Quasar_Catfish <ctsn99@gmail.com>
2020-08-05 21:10:21 -0400

    Delete Secrets.java

--- src/main/java/com/quas/mesozoicisland/util/Secrets.java
+++
@@ -1,6 +0,0 @@
-package com.quas.mesozoicisland.util;
-
-public class Secrets {
-	public static final String PROFESSOR_TOKEN = "NjQ0MzQ3MTM3NzIyODEwMzY4.XrHFqw.Hdy_ftzhp-0Ju-aes2YnSMS13U8";
-	public static final String ASSISTANT_TOKEN = "NjQ0MzAwMDA3MjU5ODk3ODU2.XcypXQ.JrDqzDnR4g6QTmx2PYsohvNtUQA";
-}
