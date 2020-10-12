5bc6869c84ed50b0ed3c6d4e71865d1b752f1682
Quas <ctsn99@gmail.com>
2020-10-11 21:56:56 -0400

    battle bug

--- src/main/java/com/quas/mesozoicisland/battle/Battle.java
+++ src/main/java/com/quas/mesozoicisland/battle/Battle.java
@@ -187,7 +187,7 @@ public class Battle {
 					// Dinosaur dopped halloween candy
 					if (Event.isEventActive(EventType.Halloween) && MesozoicRandom.nextInt(20) == 0) {
 						Item item = Item.getItem(Util.getRandomElement(Constants.HALLOWEEN_CANDY));
-						Action.sendDelayedMessage(MesozoicIsland.getAssistant().getIdLong(), time + 1500, Constants.SPAWN_CHANNEL, String.format("**%s:** %s, you found %s %s.", tier.toString(), Util.getArticle(item.toString()), item.toString()));
+						Action.sendDelayedMessage(MesozoicIsland.getAssistant().getIdLong(), time + 1500, Constants.SPAWN_CHANNEL, String.format("**%s:** %s, you found %s %s.", tier.toString(), attack.getPlayer().getAsMention(), Util.getArticle(item.toString()), item.toString()));
 						Action.addItemDelayed(attack.getPlayer().getIdLong(), time + 1500, item.getIdDmg(), 1);
 					}
 				}
