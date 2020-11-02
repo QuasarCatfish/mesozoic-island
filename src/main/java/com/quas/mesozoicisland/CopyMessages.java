f5540a8b267224d281373f5e563ef6a522e3b3ed
Quas <ctsn99@gmail.com>
2020-11-02 17:42:39 -0500

    copy messages from cave of lost hope

---
+++ src/main/java/com/quas/mesozoicisland/CopyMessages.java
@@ -0,0 +1,17 @@
+package com.quas.mesozoicisland;
+
+import com.quas.mesozoicisland.enums.DiscordChannel;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class CopyMessages extends ListenerAdapter {
+	
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		if (event.getAuthor().isBot()) return;
+		if (event.getAuthor().isFake()) return;
+
+		DiscordChannel.DebugCaveOfLostHope.getChannel(MesozoicIsland.getAssistant()).sendMessageFormat("**Message from %s:**\n> %s", event.getMember().getEffectiveName(), event.getMessage().getContentRaw()).complete();
+	}
+}
