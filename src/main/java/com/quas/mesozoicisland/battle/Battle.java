f12ce344f253a7ae19eb467b42af6c69e2b90e4a
Quas <ctsn99@gmail.com>
2020-11-06 23:40:30 -0500

    fix bug where losing in a dungeon doesnt give a loss

--- src/main/java/com/quas/mesozoicisland/battle/Battle.java
+++ src/main/java/com/quas/mesozoicisland/battle/Battle.java
@@ -324,7 +324,7 @@ public class Battle {
 					Action.addItemDelayed(attack.getPlayer().getIdLong(), time, Stat.DinosaursDefeated.getId(), 1);
 				}
 				if (defend.getPlayer().getIdLong() > CustomPlayer.getUpperLimit()) {
-					Action.addDinosaurLossDelayed(attack.getPlayer().getIdLong(), time, attack.getDinosaur().getId());
+					Action.addDinosaurLossDelayed(defend.getPlayer().getIdLong(), time, defend.getDinosaur().getId());
 				}
 				
 				// Dinosaur gains XP
