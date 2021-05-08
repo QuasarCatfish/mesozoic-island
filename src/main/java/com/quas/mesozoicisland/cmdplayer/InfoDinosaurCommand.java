ad54d0e1159f7a41d29607265da459877c1b656e
Quas <ctsn99@gmail.com>
2021-05-07 20:10:11 -0400

    reduce info command in #game

--- src/main/java/com/quas/mesozoicisland/cmdplayer/InfoDinosaurCommand.java
+++ src/main/java/com/quas/mesozoicisland/cmdplayer/InfoDinosaurCommand.java
@@ -74,38 +74,90 @@ public class InfoDinosaurCommand implements ICommand {
 		eb.setTitle(d.getEffectiveName());
 		eb.setColor(Constants.COLOR);
 		
-		if (d.getDinosaurForm() == DinosaurForm.Accursed) {
-			eb.addField(Zalgo.title("Dex Number"), Zalgo.field(d.getDex() < 0 ? "#" + Util.mult("?", Constants.MAX_DEX_DIGITS) : String.format("#%0" + Constants.MAX_DEX_DIGITS + "d", d.getDex())), true);
-			if (d.getForm() != 0) eb.addField(Zalgo.title("Form"), Zalgo.field(d.getFormName()), true);
-			eb.addField(Zalgo.title("Owner"), Zalgo.field(p.getName()), true);
-			eb.addField(Zalgo.title("Element"), Zalgo.field(d.getElement().getName()), true);
-			eb.addField(Zalgo.title("Dinosaurs Defeated"), Zalgo.field(Util.formatNumber(d.getWins())), true);
-			eb.addField(Zalgo.title("Times Defeated"), Zalgo.field(Util.formatNumber(d.getLosses())), true);
-			if (d.getItem() != null && d.getItem().getId() != 0) eb.addField(Zalgo.title("Held Item"), Zalgo.field(d.getItem().toString()), true);
-			if (d.getRune() != null && d.getRune().getId() != 0) eb.addField(Zalgo.title("Rune"), Zalgo.field(d.getRune().getName()), true);
-		} else {
-			eb.addField("Dex Number", d.getDex() < 0 ? "#" + Util.mult("?", Constants.MAX_DEX_DIGITS) : String.format("#%0" + Constants.MAX_DEX_DIGITS + "d", d.getDex()), true);
-			if (d.getDinosaurForm() != DinosaurForm.Standard) {
-				if (d.getDinosaurForm().getOwnedEmote() == null) {
-					eb.addField("Form", d.getFormName(), true);
+		if (event.getChannel().getIdLong() == DiscordChannel.Game.getIdLong()) {
+			if (d.getDinosaurForm() == DinosaurForm.Accursed) {
+				eb.addField(Zalgo.title("Dex and Form"), String.format("%s\n%s", Zalgo.field("#" + d.getId()), Zalgo.field(d.getFormName())), true);
+				eb.addField(Zalgo.title("Owner"), Zalgo.field(p.getName()), true);
+				eb.addField(Zalgo.title("Element"), Zalgo.field(d.getElement().getName()), true);
+
+				if (d.getItem() != null && d.getItem().getId() != 0) eb.addField(Zalgo.title("Held Item"), Zalgo.field(d.getItem().toString()), true);
+				if (d.getRune() != null && d.getRune().getId() != 0) eb.addField(Zalgo.title("Rune"), Zalgo.field(d.getRune().getName()), true);
+			} else {
+				if (d.getDinosaurForm() == DinosaurForm.Standard) {
+					eb.addField("Dex", "#" + d.getId(), true);
+				} else if (d.getDinosaurForm().getOwnedEmote() == null) {
+					eb.addField("Dex and Form", String.format("#%s\n%s", d.getId(), d.getFormName()), true);
 				} else {
-					eb.addField("Form", String.format("%s %s", d.getFormName(), d.getDinosaurForm().getOwnedEmote().getEmote().getAsMention()), true);
+					eb.addField("Dex and Form", String.format("#%s\n%s %s", d.getId(), d.getFormName(), d.getDinosaurForm().getOwnedEmote().getEmote().getAsMention()), true);
+				}
+				eb.addField("Owner", p.getName(), true);
+				eb.addField("Element and Rarity", String.format("%s\n%s", d.getElement().toString(), d.getRarity().getAsString()), true);
+				
+				eb.addField("Level and Rank", String.format("Level %,d + %,d XP\nRank %s + %,d RP", d.getLevel(), d.getXpMinusLevel(), d.getRankString(), d.getRp()), true);
+				eb.addField("Battle Tier", String.format("%s + %,d%%", DinoMath.getBattleTier(d).toString(), DinoMath.getNextBattleTierPercent(d)), true);
+
+				boolean item = d.getItem() != null && d.getItem().getId() != 0;
+				boolean rune = d.getRune() != null && d.getRune().getId() != 0;
+				if (item && rune) {
+					eb.addField("Held Item and Rune", String.format("%s\n%s", d.getItem().toString(), d.getRune().toString()), true);
+				} else if (item) {
+					eb.addField("Held Item", d.getItem().toString(), true);
+				} else if (rune) {
+					eb.addField("Rune", d.getRune().getName(), true);
 				}
 			}
-			eb.addField("Owner", p.getName(), true);
-			eb.addField("Element", d.getElement().getName(), true);
-			eb.addField("Rarity", d.getRarity().getAsString(), true);
-			eb.addField("Level and XP", String.format("Level %,d + %,d XP", d.getLevel(), d.getXpMinusLevel()), true);
-			eb.addField("Rank and RP", String.format("Rank %s + %,d RP", d.getRankString(), d.getRp()), true);
-			eb.addField("Battle Tier", DinoMath.getBattleTier(d).toString(), true);
-			eb.addField("Health", String.format("%,d (+%d%%)", d.getHealth(), d.getHealthMultiplier()), true);
-			eb.addField("Attack", String.format("%,d (+%d%%)", d.getAttack(), d.getAttackMultiplier()), true);
-			eb.addField("Defense", String.format("%,d (+%d%%)", d.getDefense(), d.getDefenseMultiplier()), true);
-			eb.addField("Dinosaurs Defeated", Util.formatNumber(d.getWins()), true);
-			eb.addField("Times Defeated", Util.formatNumber(d.getLosses()), true);
-			if (d.getItem() != null && d.getItem().getId() != 0) eb.addField("Held Item", d.getItem().toString(), true);
-			if (d.getRune() != null && d.getRune().getId() != 0) eb.addField("Rune", d.getRune().getName(), true);
+		} else {
+			if (d.getDinosaurForm() == DinosaurForm.Accursed) {
+				eb.addField(Zalgo.title("Dex Number"), Zalgo.field("#" + d.getId()), true);
+				eb.addField(Zalgo.title("Form"), Zalgo.field(d.getFormName()), true);
+				eb.addField(Zalgo.title("Owner"), Zalgo.field(p.getName()), true);
+
+				eb.addField(Zalgo.title("Element"), Zalgo.field(d.getElement().getName()), true);
+				eb.addField(Zalgo.title("Dinosaurs Defeated"), Zalgo.field(Util.formatNumber(d.getWins())), true);
+				eb.addField(Zalgo.title("Times Defeated"), Zalgo.field(Util.formatNumber(d.getLosses())), true);
+
+				if (d.getItem() != null && d.getItem().getId() != 0) eb.addField(Zalgo.title("Held Item"), Zalgo.field(d.getItem().toString()), true);
+				if (d.getRune() != null && d.getRune().getId() != 0) eb.addField(Zalgo.title("Rune"), Zalgo.field(d.getRune().getName()), true);
+			} else {
+				eb.addField("Dex Number", "#" + d.getId(), true);
+				if (d.getDinosaurForm() != DinosaurForm.Standard) {
+					if (d.getDinosaurForm().getOwnedEmote() == null) {
+						eb.addField("Form", d.getFormName(), true);
+					} else {
+						eb.addField("Form", String.format("%s %s", d.getFormName(), d.getDinosaurForm().getOwnedEmote().getEmote().getAsMention()), true);
+					}
+				}
+				eb.addField("Owner", p.getName(), true);
+				eb.addField("Element", d.getElement().getName(), true);
+				eb.addField("Rarity", d.getRarity().getAsString(), true);
+
+				// level
+				if (d.getXp() >= Constants.MAX_DINOSAUR_XP) {
+					eb.addField("Level and XP", String.format("Level %,d (MAX)", d.getLevel()), true);
+				} else {
+					eb.addField("Level and XP", String.format("Level %,d + %,d XP\n(%,d XP to Level Up)", d.getLevel(), d.getXpMinusLevel(), DinoMath.getXp(d.getLevel() + 1) - d.getXp()), true);
+				}
+				
+				// rank
+				if (d.getRank() >= Constants.MAX_RANK) {
+					eb.addField("Rank and RP", String.format("Rank %s + %,d RP\n(Max Rank Reached)", d.getRankString(), d.getRp()), true);
+				} else if (d.canRankup()) {
+					eb.addField("Rank and RP", String.format("Rank %s + %,d RP\n(Rankup Available)", d.getRankString(), d.getRp()), true);
+				} else {
+					eb.addField("Rank and RP", String.format("Rank %s + %,d RP\n(%,d RP to Rankup)", d.getRankString(), d.getRp(), d.getRpToRankup()), true);
+				}
+				
+				eb.addField("Battle Tier", String.format("%s + %,d%%", DinoMath.getBattleTier(d).toString(), DinoMath.getNextBattleTierPercent(d)), true);
+				eb.addField("Health", String.format("%,d (+%d%%)", d.getHealth(), d.getHealthMultiplier()), true);
+				eb.addField("Attack", String.format("%,d (+%d%%)", d.getAttack(), d.getAttackMultiplier()), true);
+				eb.addField("Defense", String.format("%,d (+%d%%)", d.getDefense(), d.getDefenseMultiplier()), true);
+				eb.addField("Dinosaurs Defeated", Util.formatNumber(d.getWins()), true);
+				eb.addField("Times Defeated", Util.formatNumber(d.getLosses()), true);
+				if (d.getItem() != null && d.getItem().getId() != 0) eb.addField("Held Item", d.getItem().toString(), true);
+				if (d.getRune() != null && d.getRune().getId() != 0) eb.addField("Rune", d.getRune().getName(), true);
+			}
 		}
+
 		
 		event.getChannel().sendMessage(eb.build()).complete();
 	}
