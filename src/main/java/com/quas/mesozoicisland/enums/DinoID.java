78aac88e3adfb317cff9fad3e1142584c6818819
Quas <ctsn99@gmail.com>
2021-06-13 15:41:25 -0400

    add chicken IDs

--- src/main/java/com/quas/mesozoicisland/enums/DinoID.java
+++ src/main/java/com/quas/mesozoicisland/enums/DinoID.java
@@ -4,8 +4,17 @@ import com.quas.mesozoicisland.util.Pair;
 
 public enum DinoID {
 
+	WildChicken8(-18),
+	WildChicken7(-17),
+	WildChicken6(-16),
+	WildChicken5(-15),
+	WildChicken4(-14),
+	WildChicken3(-13),
+	WildChicken2(-12),
+	WildChicken1(-11),
 	Turkey(-10),
-	
+
+	Chicken(0),
 	Pterodactylus(1),
 	Ichthyosaurus(2),
 	Plesiosaurus(3),
@@ -106,6 +115,8 @@ public enum DinoID {
 	Diplodocus(98),
 	Laosaurus(99),
 	Epanterias(100);
+
+	public static DinoID[] WILD_CHICKENS = new DinoID[] {WildChicken1, WildChicken2, WildChicken3, WildChicken4, WildChicken5, WildChicken6, WildChicken7, WildChicken8};
 
 	private int dex;
 	private DinoID(int dex) {
